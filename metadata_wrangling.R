d885d07cb0b6d9a8c8d7f4d4e1696cb62a8d75d7
rafapoyatos <r.poyatos@creaf.uab.es>
2019-05-15 13:31:08 +0200

    need to check order in fig ntrees, nspecies

--- metadata_wrangling.R
+++ metadata_wrangling.R
@@ -81,12 +81,77 @@ sfn_sites_type <- sfn_sites1 %>%
   )
 
 
-# 4. TODO: Dataset length --------------------------------------
+
+# 4. Percentage basal area ------------------------------------------------
+
+dataset_trees_sp <- sfn_sites_type %>%
+  right_join(sfn_allstands) %>% 
+  right_join(
+    sfn_sitespecies %>%
+      group_by(si_code) %>%
+      mutate(total_ntrees = sum(sp_ntrees,na.rm=TRUE),
+             nspecies = n_distinct(sp_name),
+             percab_measured = sum(sp_basal_area_perc,na.rm=TRUE)),
+    by='si_code'
+  ) %>% 
+    mutate(
+      typeplant=ifelse(str_detect(type,'plant'),'plant',NA))
 
 
+# Temperate
+# filter out RUS_POG_VAR
+
+species_temperate<- dataset_trees_sp %>% 
+  dplyr::filter(si_biome=='Temperate forest') %>% 
+  mutate(sp_basal_area_perc=ifelse(si_code=='RUS_POG_VAR',NA,sp_basal_area_perc),
+         percab_measured=ifelse(si_code=='RUS_POG_VAR',0,percab_measured))
+
+sites_temperate <- species_temperate %>% 
+  distinct(si_code,.keep_all =TRUE)
+
+# Box x species, not optimal
+
+ggplot() + geom_bar(aes(y = sp_basal_area_perc, x = fct_reorder(si_code,percab_measured), fill = sp_name, col=typeplant), 
+                   data = species_temperate, stat="identity") +  
+  labs(title = 'Temperate forest', x="", y="% of basal area measured with sap flow")+
+  ggplot2::ylim(0,125)+
+  scale_colour_manual(values=c("red",NA))+
+  
+  theme(legend.text = element_text(size=12),legend.position="top", legend.box = "horizontal",legend.title=element_blank(),
+        
+        axis.title.y=element_text(size=20),axis.text.y=element_text(size=16),
+        plot.title=element_text(hjust=1,size=20), axis.text.x = element_text(angle = 90, hjust = 1)) +    
+  geom_text(data=sites_temperate, aes(y = 115, x = fct_reorder(si_code,percab_measured), label = ceiling(nspecies)), size=4)+
+  geom_text(data=sites_temperate, aes(y = 125, x = fct_reorder(si_code,percab_measured), label = ceiling(total_ntrees)), size=4.5,col='blue')+
+  guides(colour=FALSE,fill = guide_legend(ncol = 9))
+  #annotate('text',label='Number of sap flow trees',x=15,y=175,fontface='bold',size=6,col='blue')+
+
+# Ok but wrong order?
 
 
+ggplot() +  geom_bar(aes(y = percab_measured, x = fct_reorder(si_code,percab_measured), col = typeplant), 
+                     data = species_temperate, stat="identity")+
+  geom_bar(aes(y = sp_basal_area_perc, x = fct_reorder(si_code,percab_measured), fill = sp_name), 
+                    data = species_temperate, stat="identity") +  
+  
+  labs(title = 'Temperate forest', x="", y="% of basal area measured with sap flow")+
+  ggplot2::ylim(0,125)+
+  scale_colour_manual(values=c("black",NA))+
+  
+  theme(legend.text = element_text(size=12),legend.position="top", legend.box = "horizontal",legend.title=element_blank(),
+        
+        axis.title.y=element_text(size=20),axis.text.y=element_text(size=16),
+        plot.title=element_text(hjust=1,size=20), axis.text.x = element_text(angle = 90, hjust = 1)) +    
+  geom_text(data=sites_temperate, aes(y = 115, x = fct_reorder(si_code,percab_measured), label = ceiling(nspecies)), size=4)+
+  geom_text(data=sites_temperate, aes(y = 125, x = fct_reorder(si_code,percab_measured), label = ceiling(total_ntrees)), size=4.5,col='blue')+
+  guides(colour=FALSE,fill = guide_legend(ncol = 9))
 
+
+# TODO: Dataset length --------------------------------------
+
+foo<- species_temperate %>% 
+  filter(si_code%in%c('BRA_CAM','USA_NWH'))
+glimpse(foo)
 
 
 
