87f36b14a9fde3a1713f992ec175af733d7169a9
MalditoBarbudo <victorgrandagarcia@gmail.com>
2020-02-05 13:25:27 +0100

    basal_area scaling plot added. alpha used to indicate the no plant mesaures

--- stand_scaling.R
+++ stand_scaling.R
@@ -58,66 +58,6 @@ sites_bor_tundra <- species_bor_tundra %>%
 
 
 # Plots ------------------------------------------------------------------
-dataset_trees_sp %>% 
-  mutate(
-    sp_basal_area_perc = ifelse(si_code == 'RUS_POG_VAR', NA, sp_basal_area_perc),
-    percab_measured = ifelse(si_code == 'RUS_POG_VAR', 0, percab_measured),
-    biomes_joined = case_when(
-      si_biome %in% c(
-        'Subtropical desert', 'Temperate grassland desert','Tropical forest savanna',
-        'Temperate rain forest','Tropical rain forest'
-      ) ~ 'Dry-Tropical',
-      si_biome %in% c('Boreal forest','Tundra') ~ 'Boreal-Tundra',
-      TRUE ~ si_biome
-    )
-  ) %>%
-  filter(biomes_joined != 'Unknown') %>%
-  ggplot() +
-  geom_bar(
-    aes(
-      x = fct_reorder(si_code, sp_basal_area_perc, sum, na.rm = TRUE),
-      y = sp_basal_area_perc, fill = sp_name, colour = sp_name
-    ),
-    stat = 'identity'
-  ) +
-  geom_text(
-    aes(
-      x = fct_reorder(si_code, percab_measured, sum, na.rm = TRUE),
-      y = 115,
-      label = nspecies
-    ),
-    size = 4,
-    data = dataset_trees_sp %>%
-      distinct(si_code, .keep_all = TRUE)
-  ) +
-  geom_text(
-    aes(
-      x = fct_reorder(si_code, percab_measured, sum, na.rm = TRUE),
-      y = 125,
-      label = total_ntrees
-    ),
-    size = 4.5, colour = 'blue',
-    data = dataset_trees_sp %>%
-      distinct(si_code, .keep_all = TRUE)
-  ) +
-  labs(title = '', x = "", y = "% of basal area measured with sap flow") +
-  guides(colour = FALSE, fill = FALSE) +
-  scale_y_continuous(expand = expand_scale(c(0,0), c(0, 10))) +
-  facet_grid(rows = vars(biomes_joined), scales = 'free_y') +
-  # coord_flip() +
-  theme_bw() +
-  theme(
-    legend.text = element_text(size = 12),
-    legend.position = "top",
-    legend.box = "horizontal",
-    legend.title = element_blank(),
-    axis.title.y = element_text(size=20),
-    axis.text.y = element_text(size=16),
-    plot.title = element_text(hjust = 0, size = 20),
-    axis.text.x = element_text(angle = 90, hjust = 1)
-  )
-
-
 # Temperate - plot
 ggplot() +
   # geom_bar(aes(y = percab_measured, x = fct_reorder(si_code,percab_measured), col = typeplant), size=1.5,
